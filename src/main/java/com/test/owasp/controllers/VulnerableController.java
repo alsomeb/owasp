52f1716be8f419f5a92cbd2f2e9f2b6f88f44b73
ThunsOJ <oscar_jidaker@icloud.com>
2023-08-23 12:19:26 +0200

    Injection går

---
+++ src/main/java/com/test/owasp/controllers/VulnerableController.java
@@ -0,0 +1,22 @@
+package com.test.owasp.controllers;
+
+import com.test.owasp.models.User;
+import com.test.owasp.services.VurnableService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("vulnerable")
+public class VulnerableController {
+    private VurnableService vulnerableService;
+
+//Går att injecta
+//    @GetMapping("user")
+//    public List<User> getUserByUsername(@RequestParam String username) {
+//        return vulnerableService.findUserByUsername(username);
+//    }
+}
---
+++ src/main/java/com/test/owasp/models/User.java
@@ -0,0 +1,4 @@
+package com.test.owasp.models;
+
+public record User(String username, String password) {
+}
