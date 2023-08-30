78da5e3a89f231f2f48d27fa3a11b03830a45367
Niklas Sankell <niklas_san@gmail.com>
2023-08-30 16:12:26 +0200

    added logs

--- src/main/java/com/test/owasp/controllers/AuthController.java
+++ src/main/java/com/test/owasp/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.test.owasp.controllers;
 
 import com.test.owasp.dtos.LoginRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/login")
+@Slf4j
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
@@ -23,12 +25,15 @@ public class AuthController {
 
     @PostMapping
     public ResponseEntity<Map<String, Boolean>> login(@RequestBody LoginRequest loginRequest) {
+        log.warn("login attempt");
         //  create a UsernamePasswordAuthenticationToken
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
 
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
-
+        if (authentication.isAuthenticated()) {
+            log.info("Success");
+        }
         var response = Map.of("loginSuccess", authentication.isAuthenticated());
         return new ResponseEntity<>(response, HttpStatus.OK);
 
